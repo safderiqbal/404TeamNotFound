f8b9e1fb8b86fd0430a139a0381e0a62d9300e9d
Mindez <mindez@gmail.com>
2015-10-25 06:47:54 +0000

    implemented giphy - still needs parsing to get gif url.

--- node_project/classes/endpoints.js
+++ node_project/classes/endpoints.js
@@ -1,10 +1,13 @@
+var http = require('http');
 var fs = require('fs');
 var guid = require('guid');
 var config = require('config');
 var unirest = require('unirest');
+var request = require('request');
 var clockwork = require('clockwork')({key: config.get('keys.clockwork')});
 
 var mashapeKey = config.get('keys.mashape');
+var giphyKey = config.get('keys.giphy');
 
 function exponentialBackOff(token, callback, wait, iterations) {
     wait = wait || 2000;
@@ -28,12 +31,41 @@ function exponentialBackOff(token, callback, wait, iterations) {
                     callback(result.body);
                 }
             } else {
-                console.log('Loop ' + iterations + 'failed');
+                console.log('Loop ' + iterations + ' failed');
                 setTimeout((function () {
                     exponentialBackOff(token, callback, wait * 2, iterations + 1)
                 }), wait);
             }
         });
+}
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function getGiphy(imageContents, callback) {
+    // call the giphy api
+    console.log('Calling giphy with '+imageContents);
+    var giphyPath = '/v1/gifs/search?q=' + imageContents.replace(/ /g,'%20') + '&api_key=' + giphyKey;
+    request.get('http://api.giphy.com' + giphyPath, function(err, response, giphyResultStr) {
+        // giphy result got - if no values, remove a random part of the string and make a new request
+        var giphyResult = JSON.parse(giphyResultStr);
+
+        if (giphyResult.pagination.count !== 0) {
+            callback(giphyResult);
+        }
+        else {
+            var imageContentsArray = imageContents.split(' ');
+            if (imageContentsArray.length === 1) {
+                getGiphy('doctor who tv sad pout', callback);
+            }
+            else {
+                var removedIndex = getRandomInt(0, imageContentsArray.length);
+                var newContents = imageContentsArray.splice(removedIndex, 1).join(' ');
+                getGiphy(newContents, callback);
+            }
+        }
+    });
 }
 
 exports.receiveImage = function (req, res) {
@@ -54,12 +86,15 @@ exports.receiveImage = function (req, res) {
                 console.log('Sending...');
                 console.log(result.status, result.headers, result.body);
                 if (result.body.token) {
+                    // delete the stored image from the server and keep checking mashape for result
                     fs.unlink('uploads/'+image+'.png');
                     console.log('Removed image ' + image + '.png');
-
                     exponentialBackOff(result.body.token, function(imageResult) {
-                        res.send(imageResult);
-                        //TODO Search Giphy API for results based on text received
+                        // image identification successful - now get the result and call giphy api
+                        var imageContents = imageResult.name;
+                        getGiphy(imageContents, function(giphyResult) {
+                            res.send(giphyResult);
+                        });
                         //TODO Send links to gifs to requested number, via SMS
                     });
                 } else {
