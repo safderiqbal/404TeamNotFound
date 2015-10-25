c8e495c6843a14235bbd3ad7ad5d31ada888a3bd
Safder Iqbal <safder.iqbal@gmail.com>
2015-10-25 07:32:38 +0000

    Removing file upload and moved to form data

--- node_project/app.js
+++ node_project/app.js
@@ -3,6 +3,7 @@ var http = require('http');
 var logger = require('morgan');
 var errorHandler = require('errorhandler');
 var methodOverride = require('method-override');
+var bodyParser = require('body-parser');
 var endpoints = require('./classes/endpoints');
 
 var app = express();
@@ -12,9 +13,13 @@ var server =  http.createServer(app);
 app.set('port', process.env.PORT || 3000);
 app.use(logger('dev'));
 app.use(methodOverride());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-app.get('/', endpoints.test);
-app.post('/image', endpoints.receiveImage);
+app.get('/', endpoints.landingPage);
+app.post('/image', endpoints.start);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
